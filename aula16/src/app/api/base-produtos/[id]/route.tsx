52ba608eaa0126db0037f3602452cafd9196de33
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-03 10:54:25 -0300

    Criando requisições através de RouterHandlers arquivos router utilizando GetAll e GetById, tentando com Thunder.

--- aula16/src/app/api/base-produtos/[id]/route.tsx
+++ aula16/src/app/api/base-produtos/[id]/route.tsx
@@ -8,6 +8,10 @@ const produtos = [
     { id: 3, nome: "teclado", preco: 130, qtd: 40 },
 ];
 
-export async function GET() {
-    return NextResponse.json(produtos);
+export async function GET(response:Response, {params}:{params:{id:number}}) {
+
+    //Criar uma forma de pesquisa na lista para identificar o produto pelo ID:
+    const produto = produtos.find( p => p.id ==  params.id);
+
+    return NextResponse.json(produto);
 }
