44804a696e5558ca7da3ca7ae6f54fb31a9147d1
anacarolcg0 <anacarolcg0@gmail.com>
2024-10-03 11:46:01 -0300

    Aula 16 pronta.

--- aula16/src/components/Rodape/Rodape.tsx
+++ aula16/src/components/Rodape/Rodape.tsx
@@ -3,7 +3,7 @@ export default function Rodape() {
     return (
         <footer>
             <h2 className="bg-background text-foreground">Rodape</h2>
-            <p>Desenvolvido por <span>pf765</span></p>
+            <p>Desenvolvido por <span>rm554669</span></p>
         </footer>
     )
 }
