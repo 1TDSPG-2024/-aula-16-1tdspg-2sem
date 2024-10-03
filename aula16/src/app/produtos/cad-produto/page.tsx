718539e2afeba67e3b0cab268e3455380f3ba821
Caepena <caepenafiel@gmail.com>
2024-10-03 12:11:18 -0300

    Criando tipagem de produtos e iniciando a implementação do cadastro de PRODUTOS utilizando endpoint POST

--- aula16/src/app/produtos/cad-produto/page.tsx
+++ aula16/src/app/produtos/cad-produto/page.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link";
+"use client"
+import { useState } from "react"
 
 export default function CadProduto() {
 
-    const produtos = [
-        { id: 1, nome: "mouse", preco: 50, qtd: 100 },
-        { id: 2, nome: "monitor", preco: 1500, qtd: 20 },
-        { id: 3, nome: "teclado", preco: 130, qtd: 40 },
-    ];
+
+    const [produto, setProduto] = useState()
+
 
     return (
         <div>
