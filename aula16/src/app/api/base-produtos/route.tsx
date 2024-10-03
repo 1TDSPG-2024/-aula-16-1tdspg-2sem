4892b778b8a08e7cf510a20abd22c31007710b91
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-10-03 10:22:23 -0300

    Exercício da aula passada

--- aula16/src/app/api/base-produtos/route.tsx
+++ aula16/src/app/api/base-produtos/route.tsx
@@ -2,8 +2,12 @@ import { NextResponse } from "next/server";
 
 //CRIAR AQUI NESTE LOCAL OU TRAZER PARA CÁ A LISTA DE
 //PRODUTOS E APRESENTAR ELA NO RETORNO DA API...
-
+const produtos = [
+  { id: 1, nome: "mouse", preco: 50, qtd: 100 },
+  { id: 2, nome: "monitor", preco: 1500, qtd: 20 },
+  { id: 3, nome: "teclado", preco: 130, qtd: 40 },
+];
 
 export async function GET() {
-    return NextResponse.json({ mensagem: "Hello World!" });
+  return NextResponse.json({ produtos });
 }
